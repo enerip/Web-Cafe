8e1a2db564cb972e188ed81692ac7890a010e42a
ariasmatr <ariana.asmat@alumnos.udit.es>
2024-10-27 01:51:47 +0200

    pg home almost done

--- js/script.js
+++ js/script.js
@@ -58,13 +58,14 @@ $(document).ready(function () {
   /* LIGHT & DARK THEME */
   $(document).ready(function () {
     const themeToggle = $("#theme-toggle");
+    const logoLight = $("#logo-light"); 
+    const logoDark = $("#logo-dark");   
     let isDarkTheme = true;
   
     const themes = {
       dark: {
         "--clr-light": "#FFF4E2",
         "--clr-dark": "#3C291B",
-        /* icon: '<i class="bi bi-moon-stars-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>', */
         icon: '<i class="bi bi-sun-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
         rotation: "90deg"
       },
@@ -72,7 +73,60 @@ $(document).ready(function () {
         "--clr-light": "#3C291B",
         "--clr-dark": "#FFF4E2",
         icon: '<i class="bi bi-moon-stars-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
-        /* icon: '<i class="bi bi-sun-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>', */
+        rotation: "0deg"
+      }
+    };
+  
+    function applyTheme(theme) {
+      $.each(theme, function (property, value) {
+        document.documentElement.style.setProperty(property, value);
+      });
+      themeToggle.html(theme.icon);
+      themeToggle.css("transform", `rotate(${theme.rotation})`); 
+      
+      // Toggle logo visibility
+      if (isDarkTheme) {
+        logoLight.show();
+        logoDark.hide();
+      } else {
+        logoLight.hide();
+        logoDark.show();
+      }
+    }
+  
+    function toggleTheme() {
+      isDarkTheme = !isDarkTheme;
+      const selectedTheme = isDarkTheme ? themes.dark : themes.light;
+      $("body").toggleClass("dark"); 
+      applyTheme(selectedTheme);
+    }
+  
+    themeToggle.on("click", toggleTheme);
+  
+    applyTheme(isDarkTheme ? themes.dark : themes.light);
+  
+    function toggleOverlay(event) {
+      $(".overlayContainer").toggleClass("active");
+    }
+});
+
+
+
+  /* $(document).ready(function () {
+    const themeToggle = $("#theme-toggle");
+    let isDarkTheme = true;
+  
+    const themes = {
+      dark: {
+        "--clr-light": "#FFF4E2",
+        "--clr-dark": "#3C291B",
+        icon: '<i class="bi bi-sun-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
+        rotation: "90deg"
+      },
+      light: {
+        "--clr-light": "#3C291B",
+        "--clr-dark": "#FFF4E2",
+        icon: '<i class="bi bi-moon-stars-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
         rotation: "0deg"
       }
     };
@@ -99,6 +153,6 @@ $(document).ready(function () {
     function toggleOverlay(event) {
       $(".overlayContainer").toggleClass("active");
     }
-  });
+  }); */
   
   
