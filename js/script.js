b23ba118760badc9c9aa67d8543325b77f53f1d4
ariasmatr <ariana.asmat@alumnos.udit.es>
2024-11-10 20:05:28 +0100

    all done
    no pienso tocar nada mas, odio bootstrap

--- js/script.js
+++ js/script.js
@@ -17,20 +17,6 @@ window.onload = function () {
     carga();
 }
 
-/* MENU RESPONSIVE */
-$(document).ready(function(){
-    console.log("i need coffee");
-  
-    $("#menu .bi-list").click(function(){
-      console.log("rip");
-      $("#menu ul").addClass("openMenu");
-    });
-  
-    $("#menu li").click(function(){
-      $("#menu ul").removeClass("openMenu");
-    });
-  });
-
 /* SEARCH BAR */
 $(document).ready(function () {
     $('#search-icon').on('click', function () {
@@ -84,7 +70,6 @@ $(document).ready(function () {
       themeToggle.html(theme.icon);
       themeToggle.css("transform", `rotate(${theme.rotation})`); 
       
-      // Toggle logo visibility
       if (isDarkTheme) {
         logoLight.show();
         logoDark.hide();
@@ -104,53 +89,4 @@ $(document).ready(function () {
     themeToggle.on("click", toggleTheme);
   
     applyTheme(isDarkTheme ? themes.dark : themes.light);
-  
-    function toggleOverlay(event) {
-      $(".overlayContainer").toggleClass("active");
-    }
 });
-
-
-
-  /* $(document).ready(function () {
-    const themeToggle = $("#theme-toggle");
-    let isDarkTheme = true;
-  
-    const themes = {
-      dark: {
-        "--clr-light": "#FFF4E2",
-        "--clr-dark": "#3C291B",
-        icon: '<i class="bi bi-sun-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
-        rotation: "90deg"
-      },
-      light: {
-        "--clr-light": "#3C291B",
-        "--clr-dark": "#FFF4E2",
-        icon: '<i class="bi bi-moon-stars-fill" style="font-size: 1.5rem; color: var(--clr-light);"></i>',
-        rotation: "0deg"
-      }
-    };
-  
-    function applyTheme(theme) {
-      $.each(theme, function (property, value) {
-        document.documentElement.style.setProperty(property, value);
-      });
-      themeToggle.html(theme.icon);
-      themeToggle.css("transform", `rotate(${theme.rotation})`); 
-    }
-  
-    function toggleTheme() {
-      isDarkTheme = !isDarkTheme;
-      const selectedTheme = isDarkTheme ? themes.dark : themes.light;
-      $("body").toggleClass("dark"); 
-      applyTheme(selectedTheme);
-    }
-  
-    themeToggle.on("click", toggleTheme);
-  
-    applyTheme(isDarkTheme ? themes.dark : themes.light);
-  
-    function toggleOverlay(event) {
-      $(".overlayContainer").toggleClass("active");
-    }
-  }); */
